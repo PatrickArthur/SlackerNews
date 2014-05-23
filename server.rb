8a39b6a7ef61371e23c483d68f8fb6646d0e2e9b
Patrick <pa@arthurfamily.ws>
2014-05-23 13:49:26 -0400

    changes

--- server.rb
+++ server.rb
@@ -25,71 +25,59 @@ def read_articles_from(csv)
 end
 
 
-get '/index' do
-  @articles = read_articles_from('articles.csv')
-  erb :index
-end
-
-
-post '/articles' do
-  errors = []
-  # arrays =[]
-
-  arrays << params[:title_name]
-
-  if params[:title_name].empty?
-    errors << "Missing Title."
-  end
-
-  if params[:url_name].empty?
-    errors << "Missing Url."
-  end
-
-  if params[:desc_name].length >20
-    errors << "Length wrong."
-  end
-
-
-  # if params[:title_name] == @articles[:title_name]
-  #   errors << "Already submitted"
-  # end
-
-  test=""
-  test2=""
-
-  if !errors.empty?
-    flash.now[:error] = errors
+  get '/index' do
     @articles = read_articles_from('articles.csv')
-
-    # test=@articles[0][:title_name]
-
-    # @articles.each do |article|
-    #   test2=article[:title_name]
-    # end
-
     erb :index
-
-    # binding.pry
-
-
-     #  if @articles[:title_name] == array
-     #   @error2 = "Title is already in"
-     #   erb :index
-     #   end
-     # end
+  end
 
 
-   else
 
-    input_string="#{params[:title_name]},#{params[:url_name]},#{params[:desc_name]}"
-    File.open('articles.csv', 'a') do |file|
-      file.puts(input_string)
+
+
+  post '/articles' do
+    errors = []
+
+    if params[:title_name].empty?
+      errors << "Missing Title."
     end
 
-    redirect '/index'
-  end
+    if params[:url_name].empty?
+      errors << "Missing Url."
+    end
 
-end
+    if params[:desc_name].length >20
+      errors << "Length wrong."
+    end
+
+
+    @articles = read_articles_from('articles.csv')
+
+
+    @articles.each do |article|
+      if article[:title_name] == params[:title_name]
+        errors << 'Title already taken'
+      end
+      if article[:url_name] == params[:url_name]
+        errors << 'URL already taken'
+      end
+    end
+
+
+    if !errors.empty?
+      flash.now[:error] = errors
+      erb :index
+
+    else
+
+      input_string="#{params[:title_name]},#{params[:url_name]},#{params[:desc_name]}"
+      File.open('articles.csv', 'a') do |file|
+        file.puts(input_string)
+      end
+
+      redirect '/index'
+      end
+    end
+
 
 
 
